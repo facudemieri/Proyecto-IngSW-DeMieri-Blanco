0fbcd13d95c8cf843a02574aeb52dc63419674cd
facudemieri <facu.demieri1@icloud.com>
2026-04-22 17:49:37 -0300

    Estructura inicial del proyecto

---
+++ Services_23DB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services_23DB
+{
+    public class Class1
+    {
+    }
+}
