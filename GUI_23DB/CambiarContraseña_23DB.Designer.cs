6d78b3d7f988068d59226354d164a76a376d9fb0
Lucio1530 <blancolucio05@gmail.com>
2026-05-07 15:43:00 -0300

    cambiar contraseña

--- GUI_23DB/CambiarContraseña_23DB.Designer.cs
+++ GUI_23DB/CambiarContraseña_23DB.Designer.cs
@@ -31,16 +31,16 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtPasswordActual = new System.Windows.Forms.TextBox();
+            this.txtPasswordNueva = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtConfirmarPassword = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.btnSalir = new System.Windows.Forms.Button();
-            this.btnLogin = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.chkMostrarActual = new System.Windows.Forms.CheckBox();
+            this.chkMostrarNueva = new System.Windows.Forms.CheckBox();
+            this.chkMostrarConfirmacion = new System.Windows.Forms.CheckBox();
             this.lblLogin = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -75,21 +75,21 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Password Actual";
             // 
-            // textBox1
+            // txtPasswordActual
             // 
-            this.textBox1.Location = new System.Drawing.Point(163, 100);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PasswordChar = '*';
-            this.textBox1.Size = new System.Drawing.Size(186, 23);
-            this.textBox1.TabIndex = 2;
+            this.txtPasswordActual.Location = new System.Drawing.Point(163, 100);
+            this.txtPasswordActual.Name = "txtPasswordActual";
+            this.txtPasswordActual.PasswordChar = '*';
+            this.txtPasswordActual.Size = new System.Drawing.Size(186, 23);
+            this.txtPasswordActual.TabIndex = 2;
             // 
-            // textBox2
+            // txtPasswordNueva
             // 
-            this.textBox2.Location = new System.Drawing.Point(163, 129);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(186, 23);
-            this.textBox2.TabIndex = 4;
+            this.txtPasswordNueva.Location = new System.Drawing.Point(163, 129);
+            this.txtPasswordNueva.Name = "txtPasswordNueva";
+            this.txtPasswordNueva.PasswordChar = '*';
+            this.txtPasswordNueva.Size = new System.Drawing.Size(186, 23);
+            this.txtPasswordNueva.TabIndex = 4;
             // 
             // label3
             // 
@@ -100,13 +100,13 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "Password Nuevo";
             // 
-            // textBox3
+            // txtConfirmarPassword
             // 
-            this.textBox3.Location = new System.Drawing.Point(163, 158);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.PasswordChar = '*';
-            this.textBox3.Size = new System.Drawing.Size(186, 23);
-            this.textBox3.TabIndex = 6;
+            this.txtConfirmarPassword.Location = new System.Drawing.Point(163, 158);
+            this.txtConfirmarPassword.Name = "txtConfirmarPassword";
+            this.txtConfirmarPassword.PasswordChar = '*';
+            this.txtConfirmarPassword.Size = new System.Drawing.Size(186, 23);
+            this.txtConfirmarPassword.TabIndex = 6;
             // 
             // label4
             // 
@@ -117,59 +117,64 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "Confirmar Password";
             // 
-            // btnSalir
+            // btnCancelar
             // 
-            this.btnSalir.BackColor = System.Drawing.Color.Orange;
-            this.btnSalir.FlatAppearance.BorderSize = 0;
-            this.btnSalir.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(91, 239);
-            this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(218, 32);
-            this.btnSalir.TabIndex = 18;
-            this.btnSalir.Text = "Cancelar";
-            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnCancelar.BackColor = System.Drawing.Color.Orange;
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(91, 239);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(218, 32);
+            this.btnCancelar.TabIndex = 18;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
-            // btnLogin
+            // btnConfirmar
             // 
-            this.btnLogin.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnLogin.FlatAppearance.BorderSize = 0;
-            this.btnLogin.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(91, 198);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(218, 35);
-            this.btnLogin.TabIndex = 17;
-            this.btnLogin.Text = "Confirmar";
-            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnConfirmar.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnConfirmar.FlatAppearance.BorderSize = 0;
+            this.btnConfirmar.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.Location = new System.Drawing.Point(91, 198);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(218, 35);
+            this.btnConfirmar.TabIndex = 17;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = false;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
             // 
-            // checkBox1
+            // chkMostrarActual
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(356, 105);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(15, 14);
-            this.checkBox1.TabIndex = 19;
-            this.checkBox1.UseMnemonic = false;
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.chkMostrarActual.AutoSize = true;
+            this.chkMostrarActual.Location = new System.Drawing.Point(356, 105);
+            this.chkMostrarActual.Name = "chkMostrarActual";
+            this.chkMostrarActual.Size = new System.Drawing.Size(15, 14);
+            this.chkMostrarActual.TabIndex = 19;
+            this.chkMostrarActual.UseMnemonic = false;
+            this.chkMostrarActual.UseVisualStyleBackColor = true;
+            this.chkMostrarActual.CheckedChanged += new System.EventHandler(this.chkMostrarActual_CheckedChanged);
             // 
-            // checkBox2
+            // chkMostrarNueva
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(356, 134);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(15, 14);
-            this.checkBox2.TabIndex = 20;
-            this.checkBox2.UseMnemonic = false;
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.chkMostrarNueva.AutoSize = true;
+            this.chkMostrarNueva.Location = new System.Drawing.Point(356, 134);
+            this.chkMostrarNueva.Name = "chkMostrarNueva";
+            this.chkMostrarNueva.Size = new System.Drawing.Size(15, 14);
+            this.chkMostrarNueva.TabIndex = 20;
+            this.chkMostrarNueva.UseMnemonic = false;
+            this.chkMostrarNueva.UseVisualStyleBackColor = true;
+            this.chkMostrarNueva.CheckedChanged += new System.EventHandler(this.chkMostarNueva_CheckedChanged);
             // 
-            // checkBox3
+            // chkMostrarConfirmacion
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(355, 163);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(15, 14);
-            this.checkBox3.TabIndex = 21;
-            this.checkBox3.UseMnemonic = false;
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.chkMostrarConfirmacion.AutoSize = true;
+            this.chkMostrarConfirmacion.Location = new System.Drawing.Point(355, 163);
+            this.chkMostrarConfirmacion.Name = "chkMostrarConfirmacion";
+            this.chkMostrarConfirmacion.Size = new System.Drawing.Size(15, 14);
+            this.chkMostrarConfirmacion.TabIndex = 21;
+            this.chkMostrarConfirmacion.UseMnemonic = false;
+            this.chkMostrarConfirmacion.UseVisualStyleBackColor = true;
+            this.chkMostrarConfirmacion.CheckedChanged += new System.EventHandler(this.chkMostrarConfirmacion_CheckedChanged);
             // 
             // lblLogin
             // 
@@ -179,6 +184,7 @@
             this.lblLogin.Size = new System.Drawing.Size(69, 17);
             this.lblLogin.TabIndex = 22;
             this.lblLogin.Text = "LOGIN: ...";
+            this.lblLogin.Click += new System.EventHandler(this.lblLogin_Click);
             // 
             // CambiarContraseña_23DB
             // 
@@ -186,16 +192,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(408, 287);
             this.Controls.Add(this.lblLogin);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.btnSalir);
-            this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.chkMostrarConfirmacion);
+            this.Controls.Add(this.chkMostrarNueva);
+            this.Controls.Add(this.chkMostrarActual);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.txtConfirmarPassword);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtPasswordNueva);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPasswordActual);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -214,16 +220,16 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtPasswordActual;
+        private System.Windows.Forms.TextBox txtPasswordNueva;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtConfirmarPassword;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button btnSalir;
-        private System.Windows.Forms.Button btnLogin;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.CheckBox chkMostrarActual;
+        private System.Windows.Forms.CheckBox chkMostrarNueva;
+        private System.Windows.Forms.CheckBox chkMostrarConfirmacion;
         private System.Windows.Forms.Label lblLogin;
     }
 }
--- GUI_23DB/CambiarContraseña_23DB.cs
+++ GUI_23DB/CambiarContraseña_23DB.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL_23DB;
+using Services_23DB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,70 @@ namespace GUI_23DB
         public CambiarContraseña_23DB()
         {
             InitializeComponent();
+        }
+
+        private UsuarioBLL_23DB usuarioBLL_23DB = new UsuarioBLL_23DB();
+        private EventoBLL_23DB eventoBLL_23DB = new EventoBLL_23DB();
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPasswordActual.Text) ||
+                string.IsNullOrEmpty(txtPasswordNueva.Text) ||
+                string.IsNullOrEmpty(txtConfirmarPassword.Text))
+            {
+                MessageBox.Show("Debe completar todos los campos.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (txtPasswordNueva.Text != txtConfirmarPassword.Text)
+            {
+                MessageBox.Show("Las contraseñas nuevas no coinciden.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (txtPasswordActual.Text == txtPasswordNueva.Text)
+            {
+                MessageBox.Show("La nueva contraseña debe ser distinta a la actual.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string dni_23DB = SessionManager_23DB.ObtenerInstancia_23DB().DNI_23DB;
+            bool resultado_23DB = usuarioBLL_23DB.CambiarClave_23DB(dni_23DB, txtPasswordActual.Text, txtPasswordNueva.Text, txtConfirmarPassword.Text);
+
+            if (!resultado_23DB)
+            {
+                MessageBox.Show("La contraseña actual es incorrecta.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            eventoBLL_23DB.RegistrarEvento_23DB(dni_23DB, "Usuarios", "Cambiar Clave", 2);
+            MessageBox.Show("Contraseña actualizada correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void chkMostrarActual_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPasswordActual.PasswordChar = chkMostrarActual.Checked ? '\0' : '*';
+        }
+
+        private void chkMostarNueva_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPasswordNueva.PasswordChar = chkMostrarNueva.Checked ? '\0' : '*';
+        }
+
+        private void chkMostrarConfirmacion_CheckedChanged(object sender, EventArgs e)
+        {
+            txtConfirmarPassword.PasswordChar = chkMostrarConfirmacion.Checked ? '\0' : '*';
+        }
+
+        private void lblLogin_Click(object sender, EventArgs e)
+        {
+            lblLogin.Text = "LOGIN: " + SessionManager_23DB.ObtenerInstancia_23DB().Nombre_23DB;
         }
     }
 }
