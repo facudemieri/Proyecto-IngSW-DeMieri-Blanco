43964bdc427bffd14a1b327f4c67bb2c0e486604
facudemieri <facu.demieri1@icloud.com>
2026-05-22 16:52:23 -0300

    -

--- DAL_23DB/Conexion_23DB.cs
+++ DAL_23DB/Conexion_23DB.cs
@@ -8,7 +8,7 @@ namespace DAL_23DB
 {
     public class Conexion_23DB
     {
-        private static string cadena_23DB = @"Server=WIN-478U2AUC3KS\SQLEXPRESS;Database=INGSW_23DB;Integrated Security=True;";
+        private static string cadena_23DB = @"Server=.;Database=INGSW_23DB;Integrated Security=True;";
 
         public static string ObtenerCadena_23DB()
         {
